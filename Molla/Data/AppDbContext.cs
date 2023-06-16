b04b3ade8c7b6fd7abea6247019938512b3183d0
Mahir <mahirgs@code.edu.az>
2023-06-17 02:23:26 +0400

    MollaRegister

---
+++ Molla/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Molla.Models;
+
+namespace Molla.Data
+{
+    public class AppDbContext : IdentityDbContext<AppUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> option) : base(option) { }
+    }
+}
---
+++ Molla/ViewModels/RegisterVM.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Molla.ViewModels
+{
+    public class RegisterVM
+    {
+        [Required(ErrorMessage = "Username is required.")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Full Name is required.")]
+        public string FullName { get; set; }
+
+        [Required(ErrorMessage = "Email Address is required.")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Confirm Password is required.")]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password), ErrorMessage = "Passwords do not match.")]
+        public string CPassword { get; set; }
+    }
+}
